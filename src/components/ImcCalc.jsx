d4e6f95aa04b606cfad2855371936b1a3a969e74
Eder Henrique <ederhscc@gmail.com>
2024-02-14 16:13:51 -0300

    updated ImcCalc.jsx for to validate height and weight

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -13,6 +13,21 @@ const ImcCalc = () => {
     setHeight("");
     setWeight("");
   };
+
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "")
+  }
+
+  const handleHeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value)
+    setHeight(updatedValue);
+  };
+
+  const handleWeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value)
+    setWeight(updatedValue);
+  };
+
   return (
     <div id="calc-container">
       <h2>Calculadora de IMC</h2>
@@ -25,7 +40,7 @@ const ImcCalc = () => {
               name="height"
               id="height"
               placeholder="Exemplo 1,75"
-              onChange={(e) => setHeight(e.target.value)}
+              onChange={(e) => handleHeightChange(e)}
               value={height}
             />
           </div>
@@ -36,14 +51,14 @@ const ImcCalc = () => {
               name="weight"
               id="weight"
               placeholder="Exemplo 70,5"
-              onChange={(e) => setWeight(e.target.value)}
+              onChange={(e) => handleWeightChange(e)}
               value={weight}
             />
           </div>
         </div>
         <div className="action-control">
           <Button id="calc-btn" text="Calcular" />
-          <Button id="clear-btn" text="Limpar" action={clearForm}/>
+          <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
     </div>
