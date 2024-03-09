217d6e6f3c85498d8b2aa7056f819cc0d5addc8c
Eder Henrique <ederhscc@gmail.com>
2024-03-09 12:55:54 -0300

    updated ImcTable component to show dinamic data

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,18 @@ function App() {
     const weightFloat = +weight.replace(",", ".");
     const heightFloat = +height.replace(",", ".");
 
-    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1)
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
 
     setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass);
+      }
+    });
+
+    if (!info) return;
   };
 
   const [imc, setImc] = useState("");
@@ -28,7 +37,11 @@ function App() {
 
   return (
     <div className="container">
-      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} />
+      )}
     </div>
   );
 }
